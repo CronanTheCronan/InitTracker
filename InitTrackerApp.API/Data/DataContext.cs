9fc68f54c54d0e180976b45d392cd4e4604fe02c
Matt Cronan <mattcronan.webdev@gmail.com>
2019-05-01 09:26:37 -0500

    Added jwt security

--- InitTrackerApp.API/Data/DataContext.cs
+++ InitTrackerApp.API/Data/DataContext.cs
@@ -8,5 +8,6 @@ namespace InitTrackerApp.API.Data
         public DataContext (DbContextOptions<DataContext> options) : base (options) { }     
         
         public DbSet<Creature> Creatures { get; set; }
+        public DbSet<User> Users { get; set; }
     }
 }
---
+++ InitTrackerApp.API/Data/IAuthRepository.cs
@@ -0,0 +1,12 @@
+using System.Threading.Tasks;
+using InitTrackerApp.API.Models;
+
+namespace InitTrackerApp.API.Data
+{
+    public interface IAuthRepository
+    {
+         Task<User> Register(User user, string password);
+         Task<User> Login(string username, string password);
+         Task<bool> UserExists(string username);
+    }
+}
